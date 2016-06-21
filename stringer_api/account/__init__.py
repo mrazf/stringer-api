04e5ab2dd40295fd9afeda4bbe5ae4dbffb23417
Maxwell Farrell <maxwellrichardfarrell@gmail.com>
2016-06-21 08:59:06 +0100

    Stop infinite loop of asking for new account statements
    Make it better

--- stringer_api/account/__init__.py
+++ stringer_api/account/__init__.py
@@ -9,27 +9,35 @@ account_api = Blueprint('account_api', __name__)
 @account_api.route("/account")
 def get_account():
     account_funds_resp = client.get_account_funds()
-    account_statement = client.get_account_statement(include_item=constants.IncludeItem['EXCHANGE'])
     account = {
         'balance': account_funds_resp['available_to_bet_balance'],
         'exposure': account_funds_resp['exposure'],
-        'tennisProfit': get_tennis_profit(account_statement),
+        'tennisProfit': get_tennis_profit(),
         'wallet': account_funds_resp['wallet']
     }
 
     return jsonify(account)
 
 
-def get_tennis_profit(account_statement):
-    all_items = account_statement.account_statement
-    while account_statement.more_available:
-        account_statement = client.get_account_statement(include_item=constants.IncludeItem['EXCHANGE'], from_record=len(account_statement.account_statement))
-        all_items.extend(account_statement.account_statement)
-
-    tennis_items = filter(is_tennis_item, all_items)
+def get_tennis_profit():
+    items = get_items()
+    tennis_items = filter(is_tennis_item, items)
     profit = reduce(lambda cumulative, item: cumulative + item.amount, tennis_items, 0.0)
 
     return profit
+
+
+def get_items(from_record=0):
+    all_items = []
+    get_more_items = True
+
+    while get_more_items:
+        account_statement = client.get_account_statement(from_record=from_record)
+        all_items.extend(account_statement.account_statement)
+        get_more_items = account_statement.more_available
+        from_record = from_record + len(account_statement.account_statement)
+
+    return all_items
 
 
 def is_tennis_item(item):
